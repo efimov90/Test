9b0c01e869e5f471ef64b7f821b4ed18ea88036a
Sergey <efimov90@gmail.com>
2020-05-23 15:41:02 +0300

    update stored procedures to return IDs

---
+++ TestData/dbo/Stored Procedures/spAddresses_Insert.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spAddresses_Insert]
+    @EmployeeId INT,
+    @Country NVARCHAR(50),
+    @Region NVARCHAR(50),
+    @City NVARCHAR(50),
+    @StreetAddress NVARCHAR(50),
+    @PostalCode NVARCHAR(10)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO dbo.Address (EmployeeId, Country, Region, City, StreetAddress, PostalCode)
+    VALUES (@EmployeeId, @Country, @Region, @City, @StreetAddress, @PostalCode);
+
+    SELECT CAST(scope_identity() AS int)
+END
---
+++ TestData/dbo/Stored Procedures/spEmployees_Insert.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spEmployees_Insert]
+	@FirstName NVARCHAR(50), 
+    @MiddleName NVARCHAR(50), 
+    @LastName NVARCHAR(50), 
+    @DateOfBirth DATE, 
+    @Department NVARCHAR(50), 
+    @About TEXT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO dbo.Employee (FirstName, MiddleName, LastName, DateOfBirth, Department, About)
+    VALUES (@FirstName, @MiddleName, @LastName, @DateOfBirth, @Department, @About);
+
+    SELECT CAST(scope_identity() AS int)
+END
